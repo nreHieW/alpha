18ab2e8dd99b485ad49c5d3aa9ab638323e90545
nreHieW <limweihern@gmail.com>
2024-05-30 14:11:34 +0800

    fix cache

--- src/lib/apiHelpers.ts
+++ src/lib/apiHelpers.ts
@@ -3,7 +3,7 @@ import { DCFInputData } from "../components/ticker-display/types";
 const baseURL = process.env.URL;
 export async function getDCFInputs(query: string) {
   const response = await fetch(
-    `${baseURL}/api/ticker?ticker=${encodeURIComponent(query)}`,
+    `${baseURL}/api/ticker?ticker=${encodeURIComponent(query)}`,{ next: { revalidate: 600 } }
   );
   let data = await response.json();
   // console.log("Getting DCF Inputs");
@@ -12,7 +12,7 @@ export async function getDCFInputs(query: string) {
 }
 export async function getPriceHistory(query: string) {
   const response = await fetch(
-    `${process.env.COMPUTE_LINK}/history?ticker=${encodeURIComponent(query)}`
+    `${process.env.COMPUTE_LINK}/history?ticker=${encodeURIComponent(query)}`, { next: { revalidate: 600 } }
   );
   let data = await response.json();
 
@@ -28,6 +28,7 @@ export async function getDCFOutput(inputData: DCFInputData) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(inputData),
+    next: { revalidate: 600 },
   });
   const data = await response.json();
   return data;
